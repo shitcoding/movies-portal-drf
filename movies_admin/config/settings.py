1e82b0dbc4df49f27e242a37b164338d06482ea3
shitcoding <shitcoder@pm.me>
2023-09-26 21:18:51 +0300

    fix: load dev dependencies only in debug mode

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
     'rest_framework',
-    'django_extensions',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,8 +49,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+if DEBUG:
+    # Add development libraries if running in debug mode
+    INSTALLED_APPS += [
+        'django_extensions',
+        'debug_toolbar',
+    ]
+
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
+
 
 ROOT_URLCONF = 'config.urls'
 
